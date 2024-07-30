039efdf9dd8d385f5a7535bd6179dd54ab5a6b78
nken3820 <nken3820@gmail.com>
2024-07-30 07:10:38 +0700

    Initial Commit

---
+++ app/Models/Employer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Employer extends Model
+{
+    use HasFactory;
+
+    public function jobs() {
+        return $this->HasMany(Job::class);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:heading>
+        Home 
+    </x-slot:heading>
+
+    <h1>Hello worlk</h1>
+
+</x-layout>
